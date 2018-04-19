8f2b8f4d86712755986ba24028a3d2a79c54d150
Ryan Oglesby <ryan.oglesby08@gmail.com>
2018-04-19 16:14:45 -0500

    Add browserslist from isomorphic starter kit and package.json

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = [
+  'last 2 Chrome versions',
+  'last 2 Firefox versions',
+  'last 2 Edge versions',
+  'last 3 Safari major versions',
+  'IE 11',
+  // Data analytics shows we have high usage with 11, 10 and 9, but no minor version specified
+  'last 3 ios_saf major versions',
+  // and_chr only has 61 as version from last 3, but we have large amount of users on 60 and 59
+  'last 2 and_chr versions',
+  'last 2 Samsung versions',
+  'Android 4'
+];
